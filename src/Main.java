0193fab6b098e9c60231c51707d2d4f5d7d610bd
Vovashprot <vova.protasenko@gmail.com>
2023-02-13 15:54:43 +0300

    Task 7

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         task3();
         task4();
         task5();
+        task6();
+        task7();
     }
     public static void task1(){
         System.out.println("Task1");
@@ -34,6 +36,18 @@ public class Main {
         System.out.println("Task5");
         for(int i = 1904; i <= 2096;i = i + 1){
             if (i % 4 == 0){System.out.println(i+" год является високосным");}
+        }
+    }
+    public static void task6(){
+        System.out.println("Task6");
+        for(int i = 7; i <= 98;i = i + 7){
+            System.out.println(i);
+        }
+    }
+    public static void task7(){
+        System.out.println("Task7");
+        for(int i = 1; i <= 512;i = i * 2){
+            System.out.println(i);
         }
     }
 }
