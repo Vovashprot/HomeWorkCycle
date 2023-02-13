af58c21799767eb7d03ad75dcfdfab99d138f4a9
Vovashprot <vova.protasenko@gmail.com>
2023-02-13 15:51:43 +0300

    Task 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
     public static void task1(){
         System.out.println("Task1");
@@ -27,6 +28,12 @@ public class Main {
         System.out.println("Task4");
         for(int i = 10; i >= (- 10);i = i - 1){
             System.out.println(i);
+        }
+    }
+    public static void task5(){
+        System.out.println("Task5");
+        for(int i = 1904; i <= 2096;i = i + 1){
+            if (i % 4 == 0){System.out.println(i+" год является високосным");}
         }
     }
 }
