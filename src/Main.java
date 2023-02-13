561ceeba111f8aee1e48ea2bd62011cb6b72e6cc
Vovashprot <vova.protasenko@gmail.com>
2023-02-13 15:43:28 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
     public static void task1(){
         System.out.println("Task1");
         for(int i = 1; i <= 10;i = i + 1){
+            System.out.println(i);
+        }
+    }
+    public static void task2(){
+        System.out.println("Task2");
+        for(int i = 10; i > 0;i = i - 1){
             System.out.println(i);
         }
     }
